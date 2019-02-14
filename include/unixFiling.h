f2ca9632c3e4cb84a0dd1aac1280e0f5794ff250
Ben Walker <bwalke05@gmail.com>
2019-02-14 17:05:01 -0500

    Documentation, and reaturn duration

--- include/unixFiling.h
+++ include/unixFiling.h
@@ -8,6 +8,6 @@
 #ifndef UNIX_FILING_
 #define UNIX_FILING_
 
-void unixReadFile(const char *file, int bytes);
+int unixReadFile(const char *file, int bytes);
 
 #endif // UNIX_FILING_
--- src/unixFiling.c
+++ src/unixFiling.c
@@ -1,21 +1,37 @@
+/**
+ * Ben Walker
+ * CIS*3110
+ * 
+ * Implementation of Unix file reading with open/read/close
+ */
+
 #include "unixFiling.h"
+#include "timer.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-void unixReadFile(const char *file, int bytes) {
+/**
+ * unixReadFile()
+ * Return the amount of time it takes to read a file using
+ * Unix system calls
+ */
+int unixReadFile(const char *file, int bytes) {
    int fd = open(file, O_RDONLY);
    if (fd == -1) {
       perror("open");
       exit(EXIT_FAILURE);
    }
 
+   startTimer(); // only time the reading
    char buff[bytes];
    while (read(fd, buff, bytes) != 0);
+   int duration = stopTimer();
 
    if (close(fd) == -1) {
       perror("close");
       exit(EXIT_FAILURE);
    }
+   return duration;
 }
