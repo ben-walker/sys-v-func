ff05b2fa4a2d894c3238715e7baf9c4cba543c2b
Ben Walker <bwalke05@gmail.com>
2019-02-14 14:05:23 -0500

    Argument processing

--- src/unixio.c
+++ src/unixio.c
@@ -7,20 +7,19 @@ const char *FILE_OPT = "-f";
 const char *BYTE_OPT = "-b";
 const char *TYPE_OPT = "-t";
 
+void incorrectUsage(const char *programName) {
+   fprintf(stderr, "Usage: %s %s filename %s bytes %s callType\n", programName, FILE_OPT, BYTE_OPT, TYPE_OPT);
+   exit(EXIT_FAILURE);
+}
+
 int main(int argc, const char *argv[]) {
    int bytes, unixCall;
    const char *file = getArgForOpt(argc, argv, FILE_OPT);
-   if (file == NULL) {
-      /* show usage statement */
-      exit(EXIT_FAILURE);
-   }
-   if (convertToNum(getArgForOpt(argc, argv, BYTE_OPT), &bytes) == EXIT_FAILURE) {
-      /* show usage statement */
-      exit(EXIT_FAILURE);
-   }
-   if (convertToNum(getArgForOpt(argc, argv, TYPE_OPT), &unixCall) == EXIT_FAILURE) {
-      /* show usage statement */
-      exit(EXIT_FAILURE);
-   }
+   if (file == NULL)
+      incorrectUsage(argv[0]);
+   if (convertToNum(getArgForOpt(argc, argv, BYTE_OPT), &bytes) == EXIT_FAILURE)
+      incorrectUsage(argv[0]);
+   if (convertToNum(getArgForOpt(argc, argv, TYPE_OPT), &unixCall) == EXIT_FAILURE)
+      incorrectUsage(argv[0]);
    return 0;
 }
