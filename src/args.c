6381cba160f77246cd9bf59f45a82bbaf1cf868d
Ben Walker <bwalke05@gmail.com>
2019-02-14 17:02:34 -0500

    Documentation

--- src/args.c
+++ src/args.c
@@ -1,19 +1,37 @@
+/**
+ * Ben Walker
+ * CIS*3110
+ * 
+ * Library to find an argument that succeeds an option in
+ * an argument list
+ */
+
 #include "args.h"
 #include <stdlib.h>
 #include <string.h>
 
-int indexOfOpt(int argc, const char *argv[], const char *arg) {
+/**
+ * indexOfArg()
+ * Returns the index of an argument that succeeds
+ * a given option in an argument array
+ */
+int indexOfArg(int argc, const char *argv[], const char *opt) {
    int i = 0;
    for (; i < argc; i += 1)
-      if (strcmp(argv[i], arg) == 0)
+      if (strcmp(argv[i], opt) == 0) // loop until we find the option
          break;
-   return i < argc - 1
-      ? i + 1
-      : -1;
+   return i < argc - 1 // is i the index of the last element?
+      ? i + 1 // if not, return the index of the next array element
+      : -1; // if it is, indicate that we didn't find it
 }
 
+/**
+ * getArgForOpt()
+ * Returns a pointer to an argument that succeeds a
+ * given option in an argument array
+ */
 const char *getArgForOpt(int argc, const char *argv[], const char *opt) {
-   int index = indexOfOpt(argc, argv, opt);
+   int index = indexOfArg(argc, argv, opt);
    return index == -1
       ? NULL
       : argv[index];
