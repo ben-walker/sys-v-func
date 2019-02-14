3f7a2f10dead5e0c1e7ce6cbb20a3e1dede8f9d9
Ben Walker <bwalke05@gmail.com>
2019-02-14 16:24:19 -0500

    Detect fopen errors

--- src/cFiling.c
+++ src/cFiling.c
@@ -1,8 +1,13 @@
 #include "cFiling.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void cReadFile(const char *file, int bytes) {
    FILE *fp = fopen(file, "r");
+   if (fp == NULL) {
+      perror("fopen");
+      exit(EXIT_FAILURE);
+   }
 
    if (bytes == 1)
       while (fgetc(fp) != EOF);
