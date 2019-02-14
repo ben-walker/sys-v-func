aa86fd75cbc12c258fb14f73271cb3bcad95c5f4
Ben Walker <bwalke05@gmail.com>
2019-02-14 17:23:16 -0500

    Documentation

--- src/validation.c
+++ src/validation.c
@@ -1,20 +1,36 @@
+/**
+ * Ben Walker
+ * CIS*3110
+ * 
+ * Library to validate command line arguments
+ */
+
 #include "validation.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * invalidFile()
+ * Returns true if file name is invalid (i.e. it doesn't
+ * exist or can't be read)
+ */
 bool invalidFile(const char *file) {
-   if (access(file, F_OK) == -1) {
+   if (access(file, F_OK) == -1) { // check if file exists
       fprintf(stderr, "File %s not found\n", file);
       return true;
    }
-   if (access(file, R_OK) == -1) {
+   if (access(file, R_OK) == -1) { // check if file has read permissions
       fprintf(stderr, "File %s does not have read permission\n", file);
       return true;
    }
    return false;
 }
 
+/**
+ * invalidBytes()
+ * Returns true if bytes argument is invalid (i.e. it's less than 1)
+ */
 bool invalidBytes(int bytes) {
    if (bytes < 1) {
       fprintf(stderr, "Bytes cannot be less than 1\n");
@@ -23,6 +39,11 @@ bool invalidBytes(int bytes) {
    return false;
 }
 
+/**
+ * invalidCallType()
+ * Returns true if call type argument is invalid (i.e. it is not
+ * either 0 or 1, corresponding to C and Unix respectively)
+ */
 bool invalidCallType(int unixCall) {
    if (unixCall != 0 && unixCall != 1) {
       fprintf(stderr, "Call type must be either 0 (C functions) or 1 (Unix I/O)\n");
@@ -31,6 +52,11 @@ bool invalidCallType(int unixCall) {
    return false;
 }
 
+/**
+ * argsInvalid()
+ * Returns true if any of the args (file, bytes, call type)
+ * are invalid
+ */
 bool argsInvalid(const char *file, int bytes, int unixCall) {
    return invalidFile(file) ||
       invalidBytes(bytes) ||
