a32e7349abb1a07435238133f94a537b09749281
Ben Walker <bwalke05@gmail.com>
2019-02-14 14:26:13 -0500

    Function to describe how the program will run

--- src/unixio.c
+++ src/unixio.c
@@ -12,6 +12,23 @@ void incorrectUsage(const char *programName) {
    exit(EXIT_FAILURE);
 }
 
+void describeUsage(const char *file, int bytes, int unixCall) {
+   const char *callStatement = unixCall == 1
+      ? "Using Unix I/O system calls to read"
+      : "Using C functions to read";
+   const char *byteTemplate = bytes == 1
+      ? "%d byte from %s"
+      : "%d bytes from %s";
+   char byteStatement[256];
+   snprintf(byteStatement, sizeof(byteStatement), byteTemplate, bytes, file);
+   const char *readStatement = unixCall == 1
+      ? "with read."
+      : bytes == 1
+         ? "with fgetc."
+         : "with fread.";
+   printf("%s %s %s\n", callStatement, byteStatement, readStatement);
+}
+
 int main(int argc, const char *argv[]) {
    int bytes, unixCall;
    const char *file = getArgForOpt(argc, argv, FILE_OPT);
@@ -21,5 +38,6 @@ int main(int argc, const char *argv[]) {
       incorrectUsage(argv[0]);
    if (convertToNum(getArgForOpt(argc, argv, TYPE_OPT), &unixCall) == EXIT_FAILURE)
       incorrectUsage(argv[0]);
+   describeUsage(file, bytes, unixCall);
    return 0;
 }
