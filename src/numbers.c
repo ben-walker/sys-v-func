1ad39c53b5af86be0839fc4ddbb5d20715ee2abf
Ben Walker <bwalke05@gmail.com>
2019-02-14 17:11:03 -0500

    Documentation

--- src/numbers.c
+++ src/numbers.c
@@ -1,12 +1,24 @@
+/**
+ * Ben Walker
+ * CIS*3110
+ * 
+ * Library to convert a string to an integer
+ */
+
 #include "numbers.h"
 #include <stdlib.h>
 #include <limits.h>
 
+/**
+ * convertToNum()
+ * Convert a string to an integer, hex or decimal
+ */
 int convertToNum(const char *val, int *numeric) {
    if (!val) return EXIT_FAILURE;
    char *end = NULL;
    long converted = strtol(val, &end, 0);
 
+   // check for possible strtol errors
    if (converted == LONG_MAX || converted == LONG_MIN || end == val)
       return EXIT_FAILURE;
    *numeric = converted;
--- src/timer.c
+++ src/timer.c
@@ -1,13 +1,30 @@
+/**
+ * Ben Walker
+ * CIS*3110
+ * 
+ * Timing library; uses a static struct timeval to track the
+ * elapsed time between startTimer() and stopTimer() calls
+ */
+
 #include "timer.h"
 #include <sys/time.h>
 
-static struct timeval start;
-static const int CONV = 1000000;
+static struct timeval start; // tracks the start time of the timer
+static const int CONV = 1000000; // microsecond conversion
 
+/**
+ * startTimer()
+ * Store current "time of day" in the static struct
+ */
 void startTimer() {
    gettimeofday(&start, NULL);
 }
 
+/**
+ * stopTimer()
+ * Return difference between current moment and start moment,
+ * in microseconds
+ */
 int stopTimer() {
    struct timeval end;
    gettimeofday(&end, NULL);
