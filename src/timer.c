3cace18dd8a59cc127d4002ec149217519379dc3
Ben Walker <bwalke05@gmail.com>
2019-02-15 11:35:02 -0500

    stdlib in timer

--- src/timer.c
+++ src/timer.c
@@ -8,6 +8,7 @@
 
 #include "timer.h"
 #include <sys/time.h>
+#include <stdlib.h>
 
 static struct timeval start; // tracks the start time of the timer
 static const int CONV = 1000000; // microsecond conversion
